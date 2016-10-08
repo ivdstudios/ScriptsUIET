f2152565ce8a9c1ee3eafdede856d787ea436b60
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-10-08 16:28:12 +0530

    Today we studied C# Classes

---
+++ ScriptingIsFun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptingIsFun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello"); 
+		// Function calls
+		ChaltaHai(); 
+		AwazKartaHai(); 
+		TailPeeetaHai(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	// 3 Function Declarations 
+	void ChaltaHai()
+	{
+		print("Yeh tractor mast chalega....apka paisa waste nahin hgao"); 
+
+
+	}
+
+
+	void AwazKartaHai()
+	{
+		print("Thodi si awaj karta hai but");
+	}
+
+
+	void TailPeeetaHai()
+	{
+		print("Diesel pe leeti hai"); 
+	}
+}
