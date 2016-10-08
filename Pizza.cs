d845d3fc8e554e06c6794ee03250f2ce9e3ecf0f
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-10-08 16:41:46 +0530

    we have introduced a variable & made a new pizza script

---
+++ Pizza.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pizza : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- ScriptingIsFun.cs
+++ ScriptingIsFun.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ScriptingIsFun : MonoBehaviour {
 
+	int x = 345; 
+
 	// Use this for initialization
 	void Start () {
 		print("Hello"); 
