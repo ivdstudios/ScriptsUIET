cb3c7ab75aa96244d0c3a186f964353b76376000
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-10-13 23:38:51 +0530

    Classes and variables

---
+++ Inventory.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+	public class GunsNAmmo{
+		public int ammo = 3; 
+		public int granade = 2; 
+		public bool arquebus = false;
+		public bool blunderbuss = false; 
+		public bool musket = false; 
+		public bool rifle = true; 
+
+		public void Purchase(int payment){ // payment = 10
+			ammo = ammo + payment; 
+			print(ammo); 
+		}
+	}
+
+	class MeleeWeapon{
+		public bool knief = true;
+		public bool hammer = false; 
+		public bool baseballBat = false; 
+		public int boomerang = 4; 
+	}
+
+	public GunsNAmmo gnm = new GunsNAmmo(); 
+	public GunsNAmmo gnm1 = new GunsNAmmo(); 
+	// public MeleeWeapon ml = new MeleeWeapon(); 
+
+
+	// Use this for initialization
+	void Start () {
+		print(gnm.ammo);
+	
+		gnm.Purchase(10);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+	
--- Pizza.cs
+++ Pizza.cs
@@ -3,9 +3,35 @@ using System.Collections;
 
 public class Pizza : MonoBehaviour {
 
+
+	/* Functions 
+	 * 
+	 *  return_type function(arguments)
+	 *  {
+	 * 	    
+	 * 
+	 *    return type 
+	 * }
+	 * 
+	 * */
+
+
+	double func()   // declaration - definition 
+	{
+		return 3.34; 
+	}
+
+
+	string func1()  // declaration - definition 
+	{
+		return "hello world";
+	}
+
+
+
 	// Use this for initialization
 	void Start () {
-	
+		func1(); 
 	}
 	
 	// Update is called once per frame
